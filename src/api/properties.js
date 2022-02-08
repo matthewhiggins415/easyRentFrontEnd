9343790c07021d6030384ad201985b421f75e9c7
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-02-08 09:08:54 -1000

    added basic api outlines for both tenants & properties

--- src/api/properties.js
+++ src/api/properties.js
@@ -2,11 +2,46 @@ import axios from 'axios'
 import apiUrl from '../apiConfig'
 
 // Create a prop
+export const createAProperty = user => {
+    return axios.post(apiUrl + '/property/', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Read a prop
+export const getAProperty = user => {
+    return axios.get(apiUrl + '/property/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Read all props (of a single user)
+export const getAllProperties = user => {
+    return axios.get(apiUrl + '/property/', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Delete a prop
+export const deleteAProperty = user => {
+    return axios.delete(apiUrl + '/property/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Edit a prop 
+export const editAProperty = user => {
+    return axios.patch(apiUrl + '/property/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
--- src/api/tenant.js
+++ src/api/tenant.js
@@ -2,11 +2,46 @@ import axios from 'axios'
 import apiUrl from '../apiConfig'
 
 // Create a tenant
+export const createATenant = user => {
+    return axios.post(apiUrl + '/tenant/', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Read a tenant
+export const getATenant = user => {
+    return axios.get(apiUrl + '/tenant/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Read all tenants (of a single user)
+export const getAllTenants = user => {
+    return axios.get(apiUrl + '/tenants/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Delete a tenant
+export const deleteATenant = user => {
+    return axios.delete(apiUrl + '/tenant/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
 
 // Edit a tenant 
+export const editATenant = user => {
+    return axios.patch(apiUrl + '/tenant/:id', {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    })
+}
