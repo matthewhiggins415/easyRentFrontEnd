a1fd70acc4e4acf30ae886e1db0ea771c6250364
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-02-07 22:41:46 -1000

    added files for prop & tenant api calls

---
+++ src/api/properties.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import apiUrl from '../apiConfig'
+
+// Create a prop
+
+// Read a prop
+
+// Read all props (of a single user)
+
+// Delete a prop
+
+// Edit a prop 
---
+++ src/api/tenant.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import apiUrl from '../apiConfig'
+
+// Create a tenant
+
+// Read a tenant
+
+// Read all tenants (of a single user)
+
+// Delete a tenant
+
+// Edit a tenant 
