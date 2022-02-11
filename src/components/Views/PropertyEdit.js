fc721cc57b6e844c00a798db435cd942ea8a97ab
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-02-11 14:14:29 -0700

    prop edit form ui added, not functional yet tho.

--- src/components/Views/PropertyEdit.js
+++ src/components/Views/PropertyEdit.js
@@ -29,6 +29,38 @@ const linkStyle = {
   'padding': "12px"
 }  
 
+const Form = styled.form`
+  width: 90%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const InputGroup = styled.div`
+  width: 30%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+`
+
+const Input = styled.input`
+  padding: 10px;
+  border: none;
+  border-bottom: 1px solid black;
+  outline: none;
+`
+
+const SubmitBtn = styled.button`
+  width: 30%;
+  padding: 10px;
+  border: 1px solid black;
+  background-color: white;
+  margin: 10px auto;
+  cursor: pointer;
+`
+
 const PropertyEdit = ({ user }) => {
   const { id } = useParams()
   const [property, setProperty] = useState({})
@@ -51,6 +83,25 @@ const PropertyEdit = ({ user }) => {
         <h1>Edit Property</h1>
         <Link style={linkStyle} to={`/property/${id}`}>❌</Link>
       </ContainerHeader>
+      <Form>
+        <InputGroup>
+          <label>Address:</label>
+          <Input placeholder={`${property.address}`} />
+        </InputGroup>
+        <InputGroup>
+          <label>Rent Due:</label>
+          <Input placeholder={`${property.DayRentDue}`} />
+        </InputGroup>
+        <InputGroup>
+          <label>Number of Units:</label>
+          <Input placeholder={`${property.numOfUnits}`} />
+        </InputGroup>
+        <InputGroup>
+          <label>Total Rent:</label>
+          <Input placeholder={`${property.totalRent}`} />
+        </InputGroup>
+        <SubmitBtn type="submit">Submit</SubmitBtn>
+      </Form>
     </Container>
   )
 }
