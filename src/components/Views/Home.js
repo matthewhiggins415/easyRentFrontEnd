8de60e62daf0db572f581b95eaa6b07564d77e40
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-02-18 21:43:38 -1000

    Home screen details added.

--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -2,12 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 const DIV = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column; 
+  align-items: center; 
+  justify-content: center;
 `
 
 const Home = () => {
   return (  
     <DIV>
       <h1>Home</h1>
+      <h1># of properties</h1>
+      <h1># of tenants</h1>
+      <h1>total monthly rents collected</h1>
+      <h1>Outstanding tasks</h1>
     </DIV>
   )
 }
