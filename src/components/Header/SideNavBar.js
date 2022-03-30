7a52de60501d25617c47d6e6b5cedada1295af56
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-03-29 15:34:17 -1000

    removed routes.

--- src/components/Header/SideNavBar.js
+++ src/components/Header/SideNavBar.js
@@ -36,10 +36,6 @@ const SideNavBar = () => {
         <Link style={linkStyle} to="/profile">Profile</Link>
         <Link style={linkStyle} to="/properties">Properties</Link>
         <Link style={linkStyle} to="/tenants">Tenants</Link>
-        <Link style={linkStyle} to="/payments">Payments</Link>
-        <Link style={linkStyle} to="/setup">Setup</Link>
-        <Link style={linkStyle} to="/settings">Settings</Link>
-        <Link style={linkStyle} to="/documents">Documents</Link>
       </LinkContainer>
     </DIV>
   )
